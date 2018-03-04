a85ecac2dcb6c2f62bc195e67e5e40de625b7468
Cole <woodford.cole@gmail.com>
2018-03-04 09:34:20 -0800

    Added pacing

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyByBoundary : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D other) {
-		Destroy(other.gameObject);
+			Destroy(other.gameObject);
 	}
 
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -42,7 +42,8 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while (true) {
 			for (int i = 0; i < hazardCount; i ++) {
-				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				//Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate(hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds(spawnWait);
